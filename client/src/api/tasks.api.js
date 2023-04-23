c88cd14d7ecad4c70610a2f9e76a04f90d5e3bbc
adiwar8 <79269003+adiwar8@users.noreply.github.com>
2023-04-23 12:46:19 +0530

    working

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
 
 export const getTasksRequest = async () => {
-    return await axios.get("http://localhost:4000/tasks");
+    return await axios.get("http://3.104.110.137:4000/tasks");
 }
 
 export const createTaskRequest = async (task) => {
-    return await axios.post("http://localhost:4000/tasks", task);
+    return await axios.post("http://3.104.110.137:4000/tasks", task);
 }
 
 export const deleteTaskRequest = async (id) => {
-    return await axios.delete(`http://localhost:4000/tasks/${id}`);
+    return await axios.delete(`http://3.104.110.137:4000/tasks/${id}`);
 }
 
 export const getTaskRequest = async (id) => {
-    return await axios.get(`http://localhost:4000/tasks/${id}`);
+    return await axios.get(`http://3.104.110.137:4000/tasks/${id}`);
 }
 
 export const updateTaskRequest = async (id, newFields) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, newFields);
+    return await axios.put(`http://3.104.110.137:4000/tasks/${id}`, newFields);
 }
 
 export const toggleTaskDoneRequest = async (id, done) => {
-    return await axios.put(`http://localhost:4000/tasks/${id}`, {
+    return await axios.put(`http://3.104.110.137:4000/tasks/${id}`, {
         done,
     });
 }
